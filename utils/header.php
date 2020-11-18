b1b2fc95105939a23092a660caf95734df96ee4b
TrieuBui1006 <buiductrieu1006@gmail.com>
2020-11-18 17:34:09 +0100

    adding session into header

--- utils/header.php
+++ utils/header.php
@@ -23,12 +23,24 @@
                 <li>
                     <a href="info.php"><span class="link-text">Info</span></a>
                 </li>
+
+                <?php if ($_SESSION['id_utilisateur'] == false) : ?>
                 <li>
                     <a href="seConnecter.php"><span class="link-text">Se connecter</span></a>
                 </li>
                 <li>
                     <a href="sinscrire.php"><span class="link-text">S'inscrire</span></a>
                 </li>
+                <?php endif; ?>
+
+                <?php if ($_SESSION['id_utilisateur'] == true) : ?>
+                <li>
+                    <a href="monCompte.php"><span class="link-text">Mon Compte</span></a>
+                </li>
+                <li>
+                    <a href="deconnection.php"><span class="link-text">Deconnection</span></a>
+                </li>
+                <?php endif; ?>
             </ul>
         </div>
     </div>
@@ -38,8 +50,16 @@
             <li><a href="index.php" class="nav-link">Accueil</a></li>
             <li><a href="organigramme.php" class="nav-link">Organigramme</a></li>
             <li><a href="info.php" class="nav-link">Info</a></li>
+
+            <?php if ($_SESSION['id_utilisateur'] == false) : ?>
             <li><a href="seConnecter.php" class="nav-link">Se connecter</a></li>
             <li><a href="sinscrire.php" class="nav-link">S'inscrire</a></li>
+            <?php endif; ?>
+
+            <?php if ($_SESSION['id_utilisateur'] == true) : ?>
+            <li><a href="monCompte.php" class="nav-link">Mon Compte</a></li>
+            <li><a href="deconnection.php" class="nav-link">Deconnection</a></li>
+            <?php endif; ?>
         </ul>
     </div>
 </header>
