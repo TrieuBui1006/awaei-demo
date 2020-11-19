5276c31cf5a6e64729978bdbff4a1f71b143590b
NGUYEN Huynh Dung <57275058+luckysuper41@users.noreply.github.com>
2020-11-19 15:30:29 +0100

    footer

--- utils/footer.php
+++ utils/footer.php
@@ -1,6 +1,59 @@
 <footer>
-    <div class="footer-content">
-        <div class="f-content1">&copy; Web Copyright 2020</div>
-        <div class="f-content2">Designed by VIETSAF</div>
-    </div>
-</footer>
+      <div class="footer-content">
+        <div class="f-content-box box-image">
+          <div class="logo-box">
+            <img src="img/logo.jpg" alt="logos">
+          </div>
+          <div class="icons-box">
+            <a href="#" class="fb"><i class="fab fa-facebook-f"></i></a>
+            <a href="#" class="twitter"><i class="fab fa-twitter"></i></a>
+            <a href="#" class="ytb"><i class="fab fa-youtube"></i></a>
+            <a href="#" class="inst"><i class="fab fa-instagram"></i></a>
+          </div>
+        </div>
+        <div class="f-content-box box-support">
+          <div class="header-box">
+            <h2>Support</h2>
+          </div>
+          <div class="list-box">
+            <ul>
+              <li>AAA</li>
+              <li>AAA</li>
+              <li>AAA</li>
+              <li>AAA</li>
+              <li>AAA</li>
+            </ul>
+          </div>
+        </div>
+        <div class="f-content-box box-info">
+          <div class="header-box">
+            <h2>Informations</h2>
+          </div>
+          <div class="list-box">
+            <ul>
+              <li>BBB</li>
+              <li>BBB</li>
+              <li>BBB</li>
+              <li>BBB</li>
+              <li>BBB</li>
+            </ul>
+          </div>
+        </div>
+        <div class="f-content-box box-email">
+          <div class="header-box">
+            <h2>Support</h2>
+          </div>
+          <div class="input-box">
+            <form>
+              <input class="email" type="email" name="email" placeholder="Type your email...">
+              <input class="submit" type="submit" value="Send">
+            </form>
+          </div>
+        </div>
+      </div>       
+
+      <div class="footer-copyright">
+        <div class="f-content">&copy; Copyright 2020</div>
+        <div class="f-content">Designed by ABC</div>
+      </div>
+    </footer>
