ef5f40588245a7aa350189ce654e75b45a401a5b
TrieuBui1006 <buiductrieu1006@gmail.com>
2020-12-02 15:57:21 +0100

    implement modbus in monCompte

--- monCompte.php
+++ monCompte.php
@@ -20,6 +20,8 @@ if ($_SESSION['role_utilisateur'] != 0 || !$_SESSION['id_utilisateur']) {
     <link rel="icon" type="image/png" href="../img/logo.jpg" />
 
     <link rel="stylesheet" href="css/style.css" />
+    <script src="./node_modules/plotly.js-dist/plotly.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <title>Les Viets - Info</title>
 </head>
 
@@ -43,105 +45,73 @@ if ($_SESSION['role_utilisateur'] != 0 || !$_SESSION['id_utilisateur']) {
         ?>
 
         <main>
-            <h1>Hello</h1>
+            <div class="wrapper">
+                <div id="chart"></div>
+            </div>
+            <div class="flexBoxTemp">
+                <div class="childBox">
+                    <h3>Salon</h3>
+                    <div class="BoxContent" id='test'>
+                        <span id='livingroom'></span>
+                    </div>
+                </div>
+                <div class="childBox">
+                    <h3>Chambre 1</h3>
+                    <div class="BoxContent">
+                        <span id='bedroom1'></span>
+                    </div>
+                </div>
 
-            <p>
-                I'm baby kale chips affogato ennui lumbersexual, williamsburg paleo quinoa
-                iceland normcore tumeric. Kitsch coloring book retro, seitan schlitz
-                tattooed biodiesel vexillologist neutra. Synth mumblecore deep v, umami
-                selfies normcore gluten-free snackwave. Seitan ramps drinking vinegar
-                venmo keytar, humblebrag VHS post-ironic tacos godard pour-over.
-            </p>
-            <p>
-                Sartorial kogi taxidermy, kickstarter synth yr irony ennui everyday carry
-                retro helvetica stumptown cloud bread squid echo park. Etsy cloud bread
-                sartorial quinoa tacos beard mumblecore shaman tumblr pop-up. Twee retro
-                fingerstache af helvetica pabst 8-bit leggings taiyaki portland ramps tbh
-                tumblr vinyl. Neutra humblebrag bushwick portland subway tile plaid, offal
-                scenester flexitarian cliche squid small batch palo santo. Palo santo meh
-                adaptogen +1 3 wolf moon, listicle brunch ethical fanny pack everyday
-                carry fam. Offal fingerstache taxidermy, man bun venmo PBR&amp;B helvetica
-                thundercats everyday carry tote bag artisan cray wolf jianbing.
-            </p>
-            <p>
-                Taxidermy thundercats whatever austin. VHS helvetica ethical, dreamcatcher
-                enamel pin YOLO shabby chic locavore man bun crucifix pabst chillwave
-                pop-up vegan. Air plant mlkshk ethical echo park tumeric, whatever
-                crucifix godard scenester locavore pork belly yuccie vape. +1 gochujang
-                put a bird on it, pork belly whatever selfies vaporware occupy banh mi
-                normcore VHS. Cornhole normcore hashtag tilde. Hell of yr try-hard DIY raw
-                denim banjo, enamel pin irony polaroid copper mug tofu. Dreamcatcher lomo
-                literally 90's before they sold out, 3 wolf moon banh mi seitan chambray
-                cliche offal tote bag occupy pug.
-            </p>
-            <p>
-                Post-ironic hot chicken salvia yr yuccie ugh cold-pressed keffiyeh franzen
-                viral taxidermy mustache slow-carb crucifix vape. Taiyaki yuccie hell of
-                tacos PBR&amp;B, kitsch meggings tbh truffaut kickstarter mixtape af kogi.
-                Fingerstache vegan tofu waistcoat gentrify cray. Drinking vinegar 3 wolf
-                moon health goth craft beer master cleanse. Letterpress health goth 8-bit
-                chillwave craft beer brooklyn. Chicharrones master cleanse 8-bit,
-                mumblecore copper mug messenger bag poutine lomo kale chips flannel. Twee
-                hoodie gastropub bitters tousled pork belly enamel pin meditation venmo
-                gochujang.
-            </p>
-            <p>
-                Next level selfies cronut ethical. Tofu tumblr you probably haven't heard
-                of them, man braid literally forage swag chillwave. Pug yr flannel
-                tumeric. Coloring book yr chillwave snackwave, shoreditch shaman gentrify
-                typewriter tumeric DIY copper mug small batch. Scenester waistcoat YOLO
-                hexagon kombucha poke 8-bit meditation. Selvage scenester forage
-                williamsburg. Hoodie fingerstache tacos mustache, hashtag quinoa next
-                level sartorial craft beer retro disrupt lo-fi.
-            </p>
-            <p>
-                YOLO twee keytar farm-to-table flexitarian cardigan polaroid lumbersexual
-                adaptogen drinking vinegar echo park dreamcatcher. Brunch shoreditch
-                dreamcatcher iPhone knausgaard plaid edison bulb letterpress ethical yr
-                fanny pack. Typewriter portland woke glossier cronut, post-ironic migas
-                gentrify letterpress cray brunch lyft 8-bit master cleanse. Pitchfork
-                thundercats organic pour-over unicorn lomo.
-            </p>
-            <p>
-                Ugh yr tacos aesthetic everyday carry, tumeric selvage cliche skateboard.
-                Wolf truffaut enamel pin vexillologist poutine. Hoodie roof party pabst,
-                cardigan letterpress af disrupt +1 subway tile chillwave live-edge
-                meggings next level readymade. Master cleanse gentrify hashtag, stumptown
-                fam single-origin coffee occupy dreamcatcher air plant viral vexillologist
-                enamel pin meggings. Tumblr chambray pickled microdosing austin scenester
-                green juice.
-            </p>
-            <p>
-                Austin four dollar toast church-key, vaporware hoodie edison bulb jean
-                shorts sustainable williamsburg plaid helvetica scenester lomo humblebrag.
-                Meditation tumblr kickstarter ennui williamsburg taiyaki pabst pour-over.
-                8-bit godard cred, chillwave enamel pin skateboard you probably haven't
-                heard of them. Meditation before they sold out single-origin coffee swag
-                try-hard jianbing slow-carb shaman leggings. Palo santo shabby chic
-                whatever man bun. Master cleanse wayfarers single-origin coffee pork belly
-                cronut, normcore cliche jianbing before they sold out tousled shabby chic
-                af pop-up gentrify. Direct trade la croix vexillologist jianbing,
-                flexitarian selvage try-hard stumptown polaroid shaman wayfarers poke
-                ramps food truck swag.
-            </p>
-            <p>
-                Pok pok lumbersexual wayfarers, direct trade leggings poutine truffaut
-                kitsch. Seitan aesthetic master cleanse squid coloring book banh mi YOLO
-                vegan locavore vexillologist readymade next level pop-up edison bulb.
-                Selvage knausgaard literally, quinoa photo booth 3 wolf moon microdosing
-                freegan yuccie. Truffaut gentrify lomo put a bird on it waistcoat. Ugh
-                austin distillery, tbh actually pork belly snackwave artisan mixtape
-                quinoa vexillologist pok pok polaroid listicle readymade.
-            </p>
-            <p>
-                Hammock letterpress prism dreamcatcher truffaut shabby chic vice
-                cold-pressed. Franzen pug fashion axe before they sold out, tumblr irony
-                kogi actually af bushwick banh mi. Snackwave bicycle rights tofu
-                dreamcatcher tote bag pour-over meditation raw denim fanny pack. Pop-up
-                retro taiyaki meditation twee gastropub VHS etsy. Semiotics gochujang
-                street art normcore, edison bulb farm-to-table pour-over taxidermy
-                brooklyn.
-            </p>
+                <div class="childBox">
+                    <h3>Chambre 2</h3>
+                    <div class="BoxContent">
+                        <span id='bedroom2'></span>
+                    </div>
+                </div>
+
+                <div class="childBox">
+                    <h3>Chambre 3</h3>
+                    <div class="BoxContent">
+                        <span id='bedroom3'></span>
+                    </div>
+                </div>
+
+                <div class="childBox">
+                    <h3>Salle de bains 1</h3>
+                    <div class="BoxContent">
+                        <span id='bathroom1'></span>
+                    </div>
+                </div>
+
+                <div class="childBox">
+                    <h3>Salle de bains 2</h3>
+                    <div class="BoxContent">
+                        <span id='bathroom2'></span>
+                    </div>
+                </div>
+
+                <div class="childBox">
+                    <h3>Cuisine</h3>
+                    <div class="BoxContent">
+                        <span id='cuisine'></span>
+                    </div>
+                </div>
+
+                <div class="childBox">
+                    <h3>Garage</h3>
+                    <div class="BoxContent">
+                        <span id='garage'></span>
+                    </div>
+                </div>
+
+                <div class="childBox">
+                    <h3>Couloir</h3>
+                    <div class="BoxContent">
+                        <span id='couloir'></span>
+                    </div>
+                </div>
+
+            </div>
         </main>
     </section>
 
@@ -150,3 +120,176 @@ if ($_SESSION['role_utilisateur'] != 0 || !$_SESSION['id_utilisateur']) {
 </body>
 
 </html>
+
+<script>
+setInterval(getTemp, 200);
+setInterval(getTime, 1000);
+setInterval(updateNameDisplay, 1000);
+
+var result_temp = 0;
+var result_time = 0;
+
+function getTemp() {
+    jQuery.ajax({
+        type: "POST",
+        url: 'utils/modbus.php',
+        dataType: 'json',
+        data: {
+            functionname: 'getTemp',
+            arguments: 0
+        },
+
+        success: function(obj, textstatus) {
+            if (!('error' in obj)) {
+                result_temp = obj.result;
+                console.log(obj);
+            } else {
+                console.log(obj.error);
+            }
+        }
+    });
+}
+
+function getTime() {
+    jQuery.ajax({
+        type: "POST",
+        url: 'utils/modbus.php',
+        dataType: 'json',
+        data: {
+            functionname: 'getTime',
+            arguments: 0
+        },
+
+        success: function(obj, textstatus) {
+            if (!('error' in obj)) {
+                result_time = obj.result;
+                console.log(result_time);
+            } else {
+                console.log(obj.error);
+            }
+        }
+    });
+}
+
+function updateNameDisplay() {
+    document.getElementById('livingroom').innerHTML = result_temp[1] / 10;
+    document.getElementById('bedroom1').innerHTML = result_temp[3] / 10;
+    document.getElementById('bedroom2').innerHTML = result_temp[3] / 10;
+    document.getElementById('bedroom3').innerHTML = result_temp[3] / 10;
+    document.getElementById('cuisine').innerHTML = result_temp[5] / 10;
+    document.getElementById('bathroom1').innerHTML = result_temp[7] / 10;
+    document.getElementById('bathroom2').innerHTML = result_temp[7] / 10;
+    document.getElementById('garage').innerHTML = result_temp[1] / 10;
+    document.getElementById('couloir').innerHTML = result_temp[5] / 10;
+}
+
+
+var varTest = 0;
+
+function getData() {
+    varTest = varTest + 1;
+    return varTest;
+}
+Plotly.plot('chart', [{
+        y: [result_temp[1] / 10],
+        type: 'line',
+        name: 'Salon'
+    },
+    {
+        y: [result_temp[3] / 10],
+        type: 'line',
+        name: 'Chambre1'
+    },
+    {
+        y: [result_temp[3] / 10],
+        type: 'line',
+        name: 'Chambre2'
+    },
+    {
+        y: [result_temp[3] / 10],
+        type: 'line',
+        name: 'Chambre3'
+    },
+    {
+        y: [result_temp[5] / 10],
+        type: 'line',
+        name: 'cuisine'
+    },
+    {
+        y: [result_temp[7] / 10],
+        type: 'line',
+        name: 'salle de bains 1'
+    },
+    {
+        y: [result_temp[7] / 10],
+        type: 'line',
+        name: 'salle de bains 2'
+    },
+    {
+        y: [result_temp[1] / 10],
+        type: 'line',
+        name: 'garage'
+    },
+    {
+        y: [result_temp[5] / 10],
+        type: 'line',
+        name: 'couloir'
+    }
+]);
+var cnt = 0;
+setInterval(function() {
+    Plotly.extendTraces('chart', {
+        y: [
+            [result_temp[1] / 10]
+        ]
+    }, [0]);
+    Plotly.extendTraces('chart', {
+        y: [
+            [result_temp[3] / 10]
+        ]
+    }, [1]);
+    Plotly.extendTraces('chart', {
+        y: [
+            [result_temp[3] / 10]
+        ]
+    }, [2]);
+    Plotly.extendTraces('chart', {
+        y: [
+            [result_temp[3] / 10]
+        ]
+    }, [3]);
+    Plotly.extendTraces('chart', {
+        y: [
+            [result_temp[5] / 10]
+        ]
+    }, [4]);
+    Plotly.extendTraces('chart', {
+        y: [
+            [result_temp[7] / 10]
+        ]
+    }, [5]);
+    Plotly.extendTraces('chart', {
+        y: [
+            [result_temp[7] / 10]
+        ]
+    }, [6]);
+    Plotly.extendTraces('chart', {
+        y: [
+            [result_temp[1] / 10]
+        ]
+    }, [7]);
+    Plotly.extendTraces('chart', {
+        y: [
+            [result_temp[5] / 10]
+        ]
+    }, [8]);
+    cnt++;
+    if (cnt > 200) {
+        Plotly.relayout('chart', {
+            xaxis: {
+                range: [cnt - 200, cnt]
+            }
+        });
+    }
+}, 50);
+</script>
